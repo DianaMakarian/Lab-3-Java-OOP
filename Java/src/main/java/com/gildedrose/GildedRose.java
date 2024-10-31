d0a3aa943b7303c6ca1168f6e66487706fe9094f
Diana <dianamakaryan2005@gmail.com>
2024-11-01 00:32:38 +0200

    replaced else to else if statement

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -36,18 +36,13 @@ class GildedRose {
                 if (item.sellIn < 0) {
                     item.increaseQualityByOne();
                 }
-            } else {
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.sellIn < 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.decreaseQualityByOne();
-                        }
-                    }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.sellIn < 0) {
+                    item.decreaseQualityByOne();
                 }
             }
         }
