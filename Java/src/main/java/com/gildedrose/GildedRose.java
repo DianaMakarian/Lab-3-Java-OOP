d23307a37ef32f17f66d726192352bd76f62c1ac
Diana <dianamakaryan2005@gmail.com>
2024-11-01 00:22:49 +0200

    replaced if statements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -32,10 +32,13 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.sellIn < 0) {
                     item.increaseQualityByOne();
-                } else {
+                }
+            } else {
+                if (item.sellIn < 0) {
+
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
                     } else {
